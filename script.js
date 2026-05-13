a3704470667c3f4779d0f23a3080f1753631528a
NomoreSMILES <nomoresmiles08@gmail.com>
2026-05-13 09:52:38 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
+
+
+var dropDown;
+
+
+function openDropDown()
+{
+   let dropDown = document.getElementById("drop");
+   dropDown.classList.toggle('dropDown')
+}
+
+var Car;
+function CarGo()
+{
+  let Car = document.getElementById("Car");
+   Car.classList.toggle('CarGo')
+}
